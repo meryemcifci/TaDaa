e60af89358b99be2303b593355e1fd3123d454d5
Meryem ÇİFCİ <meryem.cifci@ogr.sakarya.edu.tr>
2025-09-19 00:53:08 +0300

    Homepage designed and coded.

---
+++ TaDaa.EntityLayer/Concrete/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaDaa.EntityLayer.Concrete
+{
+    public class BaseEntity<T>
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; 
+        public DateTime? LastModifiedAt { get; set; } 
+        public bool IsDeleted { get; set; } = false; 
+    }
+}
---
+++ TaDaa.EntityLayer/Concrete/TaskEntry.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaDaa.EntityLayer.Concrete
+    //Bugünün TaDaa'ları
+{
+    public class TaskEntry: BaseEntity<int>
+    {
+        [Key]
+        public int TaskEntryId { get; set; }
+
+        [Required]
+        [MaxLength(500)] 
+        public string TaskDescription { get; set; }
+
+        [Required]
+        [Column(TypeName = "date")]
+        public DateTime Date { get; set; }
+
+        [Range(1, 5)] 
+        public int Rating { get; set; } 
+
+        [MaxLength(255)] 
+        public string MoodNote { get; set; }
+        
+        [ForeignKey("DailySummary")]
+        public int DailySummaryId { get; set; }
+        public DailySummary DailySummary { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public AppUser User { get; set; }
+    }
+}
